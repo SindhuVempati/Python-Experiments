73551c642532d6692b76a623fcf0148b463b1a0f
Sindhu Vempati <sv0014@mix.wvu.edu>
2018-06-11 21:29:47 -0400

    Update luckynum.py

--- luckynum.py
+++ luckynum.py
@@ -1,15 +1,16 @@
-import random
+from random import randint
 
-print "Lucky Numbers! 3 numbers will be generated."
-print "If one of them is a '5', you lose!"
-
-count = 0
-while count < 3:
-  num = random.randint(1, 6)
-  print num
-  if num == 5:
-    print "Sorry, you lose!"
+# Generates a number from 1 through 10 inclusive
+random_number = randint(1, 10)
+turn =1
+guesses_left = 3
+while guesses_left!=0:
+  print "Turn ",turn
+  guess = int(raw_input("Your guess: "))
+  if random_number == guess:
+    print "You win!"
     break
-  count += 1
+  guesses_left-=1
+  turn+=1
 else:
-  print "You win!"
+  print "You lose."
